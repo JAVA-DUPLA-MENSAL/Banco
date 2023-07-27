473d73c22d1b63782389ad56f07551e1edb59a3a
TEXTBr <FireWitch2003@outlook.com>
2023-07-26 22:02:42 -0300

    sistema de conta

--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -1,12 +1,82 @@
 package org.example;
-
+/*
+    Assunto [Data] - Nome1 - Nome2
+Repositório Conta Bancária
+- Classe Conta
+Properidade: Nome Completo
+Saldo Inicial de 100 unidades
+1 - Método Resgate: Retirar Unidade (Reduzir unidades)
+2 - Método Depósito: Adicionar Unidads (Aumentar unidades)
+3 - Saldo Disponivel para investimentos
+4 - Valor Investido = Aumentar em 5% a cada laço
+Rentabilidade de 5%
+*/
 import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Conta {
     public String nome;
 
-    public BigDecimal saldo;
+    public String conta;
 
+    public static Double saldo = 100.0;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void resgate(){
+        System.out.println(saldo);
+        System.out.println("quanto voce quer retirar?");
+
+        Double valorInput = scanner.nextDouble();
+
+        if(valorInput < saldo){
+            Double result = saldo - valorInput;
+
+            saldo = result;
+
+            System.out.println(saldo);
+        }else{
+            System.out.println("Saldo insuficiente... 190");
+        }
+
+    }
+
+    public void deposito(){
+        System.out.println(saldo);
+        System.out.println("Quanto voce quer depositar?");
+
+        Double valorInput  = scanner.nextDouble();
+
+        Double result = saldo + valorInput;
+
+        saldo = result;
+
+        System.out.println(saldo);
+
+    }
+
+    public void invertir(){
+        System.out.println("saldo atual:" + saldo);
+        System.out.println("Quanto voce quer investir?");
+
+        Double valorInput = scanner.nextDouble();
+
+        Double dale = saldo - valorInput;
+
+        saldo = dale;
+
+        Double valorInvestido =  valorInput * 0.05;
+
+        System.out.println("rendimento de:" + valorInvestido);
+
+
+    }
+
+    public Conta(String nome, String conta){
+        this.nome = nome;
+        this.conta = conta;
+    }
 
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,53 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    
+    static int opc;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer i = 8;
+        Integer opc;
+
+        Scanner scanner = new Scanner(System.in);
+
+          Conta conta = new Conta("Andre","0.0123");
+
+        //conta.resgate(saldo);
+        //conta.deposito(saldo);
+        //conta.invertir();
+        do{
+            System.out.println("          Bem vindo " + conta.nome + " Conta " + conta.conta);
+            System.out.println("==================================================");
+            System.out.println(" 1 - Resgate \n 2 - Depósito \n 3 - Investimento PIX \n 0 - SAIR");
+            System.out.println("==================================================");
+
+            opc = scanner.nextInt();
+
+            switch (opc){
+                case 1:
+                    conta.resgate();
+
+                    break;
+                case 2:
+                    conta.deposito();
+                    break;
+                case 3:
+                    conta.invertir();
+                    break;
+                case 0:
+                    System.out.println("Ate a proxima");
+                    break;
+                default:
+                    System.out.println("esta opção não existe");
+                    break;
+            }
+
+        }while(i != 0);
+
+
+
     }
+
+
 }
